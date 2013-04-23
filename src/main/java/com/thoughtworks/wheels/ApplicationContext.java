a62ee31ff42109a42558ce1d6f769907bbe29010
mzhaoTWer <zm_uestc@hotmail.com>
2013-04-23 22:14:25 +0800

    [Ming] add some functionalities in the ApplicationContext

--- src/main/java/com/thoughtworks/wheels/ApplicationContext.java
+++ src/main/java/com/thoughtworks/wheels/ApplicationContext.java
@@ -1,17 +1,21 @@
 package com.thoughtworks.wheels;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class ApplicationContext {
 
     public static final String BEAN = "bean";
+    public static final String CLASS_FULL_NAME = "class";
     public static final String BEAN_PROPERTY = "property";
     public static final String BEAN_CONSTRUCTOR_ARGS = "constructor-args";
+    public static final HashMap BEAN_MAP = new HashMap<String, Integer>();
 
     private final File file;
 
@@ -22,22 +26,37 @@ public class ApplicationContext {
     public Object getBean(String beanName) {
         Object obj = null;
         try {
-            Element beanElement = getBeanElement(beanName);
+            Element beanElement = getBeanElementByName(beanName);
+            String classFullName = beanElement.getAttribute(CLASS_FULL_NAME);
+            obj = Class.forName(classFullName).newInstance();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return obj;
     }
 
-    protected Element getBeanElement(String beanName) throws ParserConfigurationException, SAXException, IOException {
-        return (Element) XmlParser.parseXml(file).getDocumentElement().getElementsByTagName(BEAN).item(0);
+    protected void putBeanIntoBeanMap() throws ParserConfigurationException, SAXException, IOException {
+        NodeList nodeList = getBeanElements();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            BEAN_MAP.put(((Element) (nodeList.item(i))).getAttribute("id"), i);
+        }
+    }
+
+    protected Element getBeanElementByName(String beanName) throws ParserConfigurationException, SAXException, IOException {
+        putBeanIntoBeanMap();
+        return (Element) getBeanElements().item((Integer) BEAN_MAP.get(beanName));
     }
 
     protected Element getConstructorArgsElement(String beanName) throws IOException, SAXException, ParserConfigurationException {
-        return (Element) getBeanElement(beanName).getElementsByTagName(BEAN_CONSTRUCTOR_ARGS).item(0);
+        return (Element) getBeanElementByName(beanName).getElementsByTagName(BEAN_CONSTRUCTOR_ARGS).item(0);
     }
 
-    protected Element getPropertiesElement(String beanName) throws IOException, SAXException, ParserConfigurationException {
-        return (Element) getBeanElement(beanName).getElementsByTagName(BEAN_PROPERTY).item(0);
+    protected Element getPropertyElements(String beanName) throws IOException, SAXException, ParserConfigurationException {
+        return (Element) getBeanElementByName(beanName).getElementsByTagName(BEAN_PROPERTY).item(0);
+    }
+
+    private NodeList getBeanElements() throws ParserConfigurationException, SAXException, IOException {
+        return XmlParser.parseXml(file).getDocumentElement().getElementsByTagName(BEAN);
     }
 }
--- src/test/java/com/thoughtworks/wheels/ApplicationContextTest.java
+++ src/test/java/com/thoughtworks/wheels/ApplicationContextTest.java
@@ -10,6 +10,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class ApplicationContextTest {
 
@@ -26,11 +27,25 @@ public class ApplicationContextTest {
     public void get_bean_id_and_class_from_xml() throws ParserConfigurationException, SAXException, IOException {
 
         //when
-        Element bean = applicationContext.getBeanElement("customer");
+        Element bean = applicationContext.getBeanElementByName("customer");
 
         //then
         Assert.assertThat(bean.getAttribute("id"), Matchers.is("customer"));
         Assert.assertThat(bean.getAttribute("class"), Matchers.is("com.thoughtworks.wheels.Customer"));
+    }
+
+    @Test
+    public void put_beans_from_xml_into_map() throws ParserConfigurationException, SAXException, IOException {
+        //given
+        HashMap expectedMap = new HashMap<>();
+        expectedMap.put("customer", 0);
+        expectedMap.put("name", 1);
+
+        //when
+        applicationContext.putBeanIntoBeanMap();
+
+        //then
+        Assert.assertEquals(expectedMap, applicationContext.BEAN_MAP);
     }
 
     @Test
@@ -43,15 +58,15 @@ public class ApplicationContextTest {
         Assert.assertThat(constructorArgs.getAttribute("value"), Matchers.is("0001"));
     }
 
-//    @Test
-//    public void generate_a_bean_according_to_xml() throws Exception {
-//
-//        //when
-//        Name name = (Name)applicationContext.getBean("name");
-//
-//        //then
-//        Assert.assertThat(name.getFirstName(), Matchers.is("Ming"));
-//        Assert.assertThat(name.getLastName(), Matchers.is("Zhao"));
-//        Assert.assertThat(name.getNickName(), Matchers.is("xiaoming"));
-//    }
+    @Test
+    public void generate_a_bean_according_to_xml() throws Exception {
+
+        //when
+        Name name = (Name) applicationContext.getBean("name");
+
+        //then
+        Assert.assertThat(name.getFirstName(), Matchers.is("Ming"));
+        Assert.assertThat(name.getLastName(), Matchers.is("Zhao"));
+        Assert.assertThat(name.getNickName(), Matchers.is("xiaoming"));
+    }
 }
